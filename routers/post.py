fd94330ca3b657af9f05278db5d7f6a0be59611e
valkorostyslav <enyway.valko.08.2004@gmail.com>
2024-11-11 16:06:54 +0200

    Initial commit

---
+++ routers/post.py
@@ -0,0 +1,51 @@
+import random
+import shutil
+import string
+from typing import List
+
+from fastapi import HTTPException, UploadFile, File
+from fastapi import APIRouter, Depends, status
+from sqlalchemy.orm import Session
+
+from auth.oauth2 import get_current_user
+from db import db_post
+from db.database import get_db
+from routers.schemas import PostBase, PostDisplay
+
+from routers.schemas import UserAuth
+
+
+router = APIRouter(
+    prefix='/post',
+    tags=['post']
+)
+
+image_url_type = ['absolute', 'relative']
+
+@router.post('', response_model=PostDisplay)
+def create(request: PostBase, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+    if not request.image_url_type in image_url_type:
+        raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+                            detail="Parameter image_url_type can only take values 'absolute' or 'relative'.")
+    return db_post.create(db, request)
+
+@router.get('/all', response_model=List[PostDisplay])
+def posts(db: Session = Depends(get_db)):
+    return db_post.get_all(db)
+
+@router.post('/image')
+def upload_image(image: UploadFile = File(...), current_user: UserAuth = Depends(get_current_user)):
+    letters = string.ascii_letters
+    rand_str = ''.join(random.choice(letters) for i in range(6))
+    new = f'_{rand_str}.'
+    filename = new.join(image.filename.rsplit('.', 1))
+    path = f'images/{filename}'
+
+    with open(path, 'w+b') as buffer:
+        shutil.copyfileobj(image.file, buffer)
+
+    return {'filename': path}
+
+@router.get('/delete/{id}')
+def delete(id: int, db: Session = Depends(get_db), current_user: UserAuth = Depends(get_current_user)):
+    return db_post.delete(db, id, current_user.id)
---
+++ routers/schemas.py
@@ -0,0 +1,57 @@
+from datetime import datetime
+from typing import List
+
+from pydantic import BaseModel
+
+class UserBase(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserDisplay(BaseModel):
+    username: str
+    email: str
+    class Config():
+        orm_mode = True
+
+class PostBase(BaseModel):
+    image_url: str
+    image_url_type: str
+    caption: str
+    creator_id: int
+
+# For PostDisplay
+class User(BaseModel):
+    username: str
+    class Config():
+        orm_mode = True
+
+# For postDisplay
+class Comment(BaseModel):
+    text: str
+    username: str
+    timestamp: datetime
+    class Config():
+        orm_mode = True
+
+class PostDisplay(BaseModel):
+    id: int
+    image_url: str
+    image_url_type: str
+    caption: str
+    timestamp: datetime
+    user: User
+    comments: List[Comment]
+    class Config():
+        orm_mode = True
+
+class UserAuth(BaseModel):
+    id: int
+    username: str
+    email: str
+
+class CommentBase(BaseModel):
+    username: str
+    text: str
+    comment: str
+    post_id: int
